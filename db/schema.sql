353567e6cb6e8d956e435608e34e46d2f5ef1755
alexhstrickland <alexhstrickland@gmail.com>
2021-03-08 17:19:58 -0500

    added schema to db folder

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Drops the db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "burgers_db" database --
+CREATE DATABASE burgers_db;
+-- Uses the "burgers_db" database --
+USE burgers_db;
+
+
+-- Create "burgers" table --
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
