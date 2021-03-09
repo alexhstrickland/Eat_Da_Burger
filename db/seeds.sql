05ae52b76431d367acf8e57ca867f56363eedc91
alexhstrickland <alexhstrickland@gmail.com>
2021-03-09 08:01:24 -0500

    changed one burger to be not devoured

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burger_name (name, devoured) VALUES ('Jalepeno Popper Burger', true);
 INSERT INTO burger_name (name, devoured) VALUES ('Black & Blue Burger', true);
-INSERT INTO burger_name (name, devoured) VALUES ('Bacon Cheeseburger Burger', true);
+INSERT INTO burger_name (name, devoured) VALUES ('Bacon Cheeseburger Burger', false;
