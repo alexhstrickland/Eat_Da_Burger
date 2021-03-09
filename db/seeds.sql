443fffd55be2043ca6c8c65c666d94fcdc33fe51
alexhstrickland <alexhstrickland@gmail.com>
2021-03-09 08:32:06 -0500

    fixed another error in file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Jalepeno Popper Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Black & Blue Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger Burger', false;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger Burger', false);
