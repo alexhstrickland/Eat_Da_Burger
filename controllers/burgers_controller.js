a7d1b5a8d0c39451f5da882c9534df69274ac903
alexhstrickland <alexhstrickland@gmail.com>
2021-03-09 19:25:12 -0500

    fixed spacing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,10 +21,7 @@ router.get('/', (req, res) => {
   
   router.put('/api/burgers/:id', (req, res) => {
   
-    burger.updateOne(
-        req.body,
-        req.params.id,
-      (result) => {
+    burger.updateOne(req.body, req.params.id, (result) => {
         if (result.changedRows === 0) {
           // If no rows were changed, then the ID must not exist, so 404
           return res.status(404).end();
