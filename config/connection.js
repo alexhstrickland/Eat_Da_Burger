30ed5eb4dda448e2dbab66f1b178a4586fe35621
alexhstrickland <alexhstrickland@gmail.com>
2021-03-09 19:17:39 -0500

    previous commit

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // MySQL connection
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '12345678',
-  database: 'burges_db',
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '12345678',
+    database: 'burger_db',
 });
+};
 
 // Make connection
 connection.connect((err) => {
